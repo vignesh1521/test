6e7a33c6545ad3fbaa29126bbbb8e8f227529a3a
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2026-09-16 07:18:52 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,20 @@
 fetch("http://lshq47lharsppiz39l18lv5fd6jx7uvj.oastify.com")
-<link rel="stylesheet" href="//s.<collab>/x.css">
-<img src="//i.<collab>/x.png">
+(function () {
+  var CSS = 'https://s.10z6cntxi705xy7jh19otbdvlmrdfc31.oastify.com/x.css';
+  var IMG = 'https://i.10z6cntxi705xy7jh19otbdvlmrdfc31.oastify.com/x.png';
+
+  var l = document.createElement('link');
+  l.rel = 'stylesheet';
+  l.type = 'text/css';
+  l.href = CSS;
+  (document.head || document.documentElement).appendChild(l);
+
+  var i = document.createElement('img');
+  i.src = IMG;
+  i.width = 1;
+  i.height = 1;
+  (document.body || document.documentElement).appendChild(i);
+
+  new Image().src = IMG + '?dup=1';
+})();
    
