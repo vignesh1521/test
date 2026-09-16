2f33bb56410c63d2d14115145a29de179da9cd68
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2026-09-16 07:54:10 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,10 +1,4 @@
-var f1 = document.createElement('iframe');
-f1.src = 'http://127.0.0.1:8080/';
-f1.width = '800';
-f1.height = '300';
-document.body.appendChild(f1);
-
-var i = document.createElement('div');
-i.innerText = 'hey there';
+var i = document.createElement('link');
+i.set
 document.body.appendChild(i);
 
