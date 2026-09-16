d9f834c71e220a5d79560cfbc248004e0702ee42
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2026-09-16 06:28:12 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,7 @@
 fetch("http://lshq47lharsppiz39l18lv5fd6jx7uvj.oastify.com")
 
+<script>
+var i = document.createElement('img');
+i.src = 'http://lshq47lharsppiz39l18lv5fd6jx7uvj.oastify.com';
+document.body.appendChild(i);
+</script>
