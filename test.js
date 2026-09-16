ac7d37777b22ba13dc58f15b1be211f900f883bc
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2026-09-16 07:56:30 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,9 @@
-var i = document.createElement('link');
-i.set
-document.body.appendChild(i);
+    var i = document.createElement('link');
+        i.setAttribute("rel", "stylesheet")
+        i.setAttribute("href", "https://vignesh1521.github.io/test/index.css")
 
+        document.body.appendChild(i);
+
+        var ele = document.createElement('h1');
+        ele.innerText="hey there"
+        document.body.appendChild(ele);
