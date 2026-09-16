5cd5848d7c8b9a64808a1b064490d9828f4a0a35
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2026-09-16 08:07:31 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
-    var i = document.createElement('link');
-        i.setAttribute("rel", "stylesheet")
-        i.setAttribute("href", "https://vignesh1521.github.io/test/index.css")
 
-        document.body.appendChild(i);
-
-        var ele = document.createElement('h1');
-        ele.innerText="hey there"
-        document.body.appendChild(ele);
+var hosts = [
+  'github.io','raw.githubusercontent.com','gstatic.com','google.com',
+  'webhook.site','pipedream.net','interact.sh','requestbin.net','oastify.com',
+  's3.amazonaws.com','cloudfront.net','azurewebsites.net',
+  '169.254.169.254','127.0.0.1'
+];
+var root = document.body || document.documentElement;
+var pre = document.createElement('pre');
+pre.innerText = 'PROBE RESULTS\n';
+root.appendChild(pre);
+hosts.forEach(function(h){
+  fetch('http://' + h + '/?_=' + Math.random(), {mode:'no-cors'})
+    .then(function(){ pre.innerText += 'OPEN    ' + h + '\n'; })
+    .catch(function(){ pre.innerText += 'BLOCKED ' + h + '\n'; });
+});
