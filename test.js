470ae9b9b8cbd885c3b671876e40f65a69fd929a
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2026-09-16 06:17:48 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,2 +1,2 @@
-//fetch("http://lshq47lharsppiz39l18lv5fd6jx7uvj.oastify.com")
-fetch("https://webhook.site/450b42b9-502c-4ca1-bcff-3d4fb0f4b907")
+fetch("http://lshq47lharsppiz39l18lv5fd6jx7uvj.oastify.com")
+
