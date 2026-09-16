cea1fadc870a868c51c418aca579d1b304357284
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2026-09-16 06:40:13 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 fetch("http://lshq47lharsppiz39l18lv5fd6jx7uvj.oastify.com")
 
 
-var i = document.createElement('img');
-i.src = 'http://lshq47lharsppiz39l18lv5fd6jx7uvj.oastify.com';
+var i = document.createElement('div');
+i.innerText = 'http://lshq47lharsppiz39l18lv5fd6jx7uvj.oastify.com';
 document.body.appendChild(i);
